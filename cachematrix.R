a2b5da99b7acbda2ad9336662f9572f4b8d19656
Crawbyte <cristian.roso@gmail.com>
2020-07-19 10:59:28 -0400

    First try for assignment for the JHU program. Problems with defining conditional numbers for the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix to be used
+basicMatrix <- matrix(1:4, nrow= 2, ncol = 2)
+useMatrix <- makeCacheMatrix(basicMatrix)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Make an object that stores the value of the matrix and its inverse to be used
+makeCacheMatrix <- function(x = numeric()) {
+        inv <- NULL
+        #Set the value of the matrix
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        #Get the value of the matrix
+        get <- function() (x)
+        #Set the value of the inverse of the matrix
+        setInverse <- function(inverse) (inv <<- inverse)
+        #Get the value of the inverse of the matrix
+        getInverse <- function() (inv)
+        #List of the function components
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Function that checks if the matrix was cached correctly
+cacheSolve <- function(x, ...){
+        inv <- x$getInverse()
+        # Condition that checks if the inverse matrix has already been cached
+        if(!is.null(inv)){
+                # And return that message if it's true
+                message("Getting cached matrix")
+                # If it's false make again the calculation
+                return(inv)
+        }
+        mat <- x$get()
+        # With this function
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
+        
 }
